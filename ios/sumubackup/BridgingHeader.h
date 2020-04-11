d4298740aacf6ae706048515eaa0cfe6ec70bbc9
Hamik Mukelyan <hamik@caltech.edu>
2020-04-11 10:24:47 -0700

    Revert "Removed sha and crypto stuff, including bridging header file and build setting. Didn't need SHAs anymore b/c I'm identifiying images with timestamp-username tuples"
    This reverts commit fd59d0cbdae99055b4ea3d6ef356529aa7b8a04a.

---
+++ ios/sumubackup/BridgingHeader.h
@@ -0,0 +1,14 @@
+//
+//  BridgingHeader.h
+//  sumubackup
+//
+//  Created by Hamik on 4/4/20.
+//  Copyright © 2020 Sumu. All rights reserved.
+//
+
+#ifndef BridgingHeader_h
+#define BridgingHeader_h
+
+#import <CommonCrypto/CommonHMAC.h>
+
+#endif /* BridgingHeader_h */
